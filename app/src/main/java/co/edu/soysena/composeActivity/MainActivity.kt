9fc0cd7f716b445ff45425c68d0b62dfff9aae62
Andres Cordoba <andres.jcv.678@gmail.com>
2023-08-16 11:48:02 -0500

    Almost finished the basic aspcts of jetpack compose

--- app/src/main/java/co/edu/soysena/composeActivity/MainActivity.kt
+++ app/src/main/java/co/edu/soysena/composeActivity/MainActivity.kt
@@ -3,211 +3,89 @@ package co.edu.soysena.composeActivity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import co.edu.soysena.composeActivity.ui.theme.ComposeActivityTheme
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.border
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.clickable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.shape.AbsoluteCutCornerShape
-import androidx.compose.foundation.shape.AbsoluteRoundedCornerShape
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.ui.geometry.CornerRadius
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.material3.Button
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import co.edu.soysena.composeActivity.ui.theme.ComposeActivityTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeActivityTheme {
-                //Columns and rows
-                ColumnsAndRows()
-
-            }
-        }
-    }
-}
-
-// COLUMNS AND ROWS
-@Composable
-@Preview
-private fun ColumnsAndRows() {
-    Surface(color = MaterialTheme.colorScheme.background) {
-        Column{
-
-            //First Row
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)
-                .background(MaterialTheme.colorScheme.primary)
-            ){
-                Text("Esta es una fila.")
-            }
-
-            //Divider between rows
-            Spacer(modifier = Modifier .padding(vertical = 5.dp))
-
-            //Second Row
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp)
-                .background(MaterialTheme.colorScheme.primary)
-            ){
-
-                Column(modifier = Modifier
-                    .width(50.dp)
-                    .height(50.dp)
-                    .background(MaterialTheme.colorScheme.secondary)
-                ){
-                    Text("Esta es una columna.")
-                }
-
-                Spacer(modifier = Modifier .width(5.dp))
-
-                Column(modifier = Modifier
-                    .width(50.dp)
-                    .height(50.dp)
-                    .background(MaterialTheme.colorScheme.secondary)
-                ){
-                    Text("Esta es una columna.")
-                }
-
-                Spacer(modifier = Modifier .width(5.dp))
-
-                Column(modifier = Modifier
-                    .width(50.dp)
-                    .height(50.dp)
-                    .background(MaterialTheme.colorScheme.secondary)
-                ){
-                    Text("Esta es una columna.")
-                }
-
-                Spacer(modifier = Modifier .width(5.dp))
-
-                Column(modifier = Modifier
-                    .width(50.dp)
-                    .height(50.dp)
-                    .background(MaterialTheme.colorScheme.secondary)
-                ){
-                    Text("Esta es una columna.")
-                }
-
-                Spacer(modifier = Modifier .width(5.dp))
-
-                Column(modifier = Modifier
-                    .width(50.dp)
-                    .height(50.dp)
-                    .background(MaterialTheme.colorScheme.secondary)
-                ){
-                    Text("Esta es una columna.")
-                }
-
-                Spacer(modifier = Modifier .width(5.dp))
-
-            }
-        }
-    }
-} // END COLUMNS AND ROWS
-
-// CARDS WHITH BUTTOMS
-@Composable
-private fun Greeting(name: String) {
-
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
-        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
-    ) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(text = "¡Buenas! ")
-                Text(text = name)
-            }
-            ElevatedButton(
-                onClick = { /* TODO */ }
-            ) {
-                Text("Mostrar mas")
+            ComposeActivityTheme() {
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun MyApp(
-    modifier: Modifier = Modifier,
-    names: List<String> = listOf("Hernando", "Ronaldo")
+fun MyApp(modifier: Modifier = Modifier) {
+
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
+
+    Surface(modifier) {
+        if (shouldShowOnboarding) {
+            OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name = name)
-        }
-    }
-}
-
-
-
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-fun DefaultPreview() {
-    ComposeActivityTheme {
-        MyApp()
-    }
-}
-
-// END CARD WHITH BUTTOMS
-
-// ELEVATION OF STATE
-
-@Composable
-fun OnboardingScreen(modifier: Modifier = Modifier) {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
 
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("¡BIENVENIDO! Esta es una aplicacion creada con Jetpack Compose.")
+        Text("¡Bienvenido a Jetpack Compose!")
         Button(
             modifier = Modifier.padding(vertical = 24.dp),
-            onClick = { shouldShowOnboarding = false }
+            onClick = onContinueClicked
         ) {
-            Text("Continue")
+            Text("Continuar")
+        }
+    }
+
+}
+
+@Composable
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names: List<String> = List(1000) { "$it" }
+) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
+            Greeting(name = name)
         }
     }
 }
@@ -215,9 +93,61 @@ fun OnboardingScreen(modifier: Modifier = Modifier) {
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun OnboardingPreview() {
-    ComposeActivityTheme {
-        OnboardingScreen()
+    ComposeActivityTheme() {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
 
-// END ELEVATION OF STATE
+@Composable
+private fun Greeting(name: String) {
+
+    var expanded by remember { mutableStateOf(false) }
+
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+            ) {
+                Text(text = "Buenos dias, ")
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
+            }
+            ElevatedButton(
+                onClick = { expanded = !expanded }
+            ) {
+                Text(if (expanded) "Mostrar menos" else "Mostrar mas")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    ComposeActivityTheme() {
+        Greetings()
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    ComposeActivityTheme() {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
